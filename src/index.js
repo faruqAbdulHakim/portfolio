3f76f7de314e537e5b61afb3e8613efe369b27ca
faruqAbdulHakim <faruqabdulhakim2001@gmail.com>
2022-12-06 09:58:16 +0700

    setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client';
+import Portfolio from './Portfolio';
+import './index.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Portfolio />)
