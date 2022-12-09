64a3b7aca61ea45eeeab34f120ef60628e9dd5f9
faruqAbdulHakim <faruqabdulhakim2001@gmail.com>
2022-12-10 06:37:43 +0700

    home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,7 +3,9 @@ import styles from './Home.module.css';
 const Home = () => {
   return (
     <div className={styles.home}>
-      <h1>Home</h1>
+      <h1 className={styles.name}>Faruq Abdul Hakim</h1>
+      <p className={styles.desc1}>Front End</p>
+      <p className={styles.desc2}>Developer</p>
     </div>
   );
 };
