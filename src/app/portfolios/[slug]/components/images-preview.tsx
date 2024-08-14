784fd44b7cc9d7a5d69a4eac02814c5e96fdb0d0
faruqAbdulHakim <faruqabdulhakim2001@gmail.com>
2024-08-15 05:47:40 +0700

    fix: adjust image preview on portfolios page

--- src/app/portfolios/[slug]/components/images-preview.tsx
+++ src/app/portfolios/[slug]/components/images-preview.tsx
@@ -6,7 +6,6 @@ import { useEffect, useRef, useState } from 'react';
 // Components
 import ImagePreviewModal from '@/components/image-preview-modal';
 import { Image } from '@nextui-org/react';
-import NextImage from 'next/image';
 
 type Props = {
   images: string[];
@@ -16,13 +15,26 @@ type Props = {
 export default function ImagesPreview({ images, title }: Props) {
   const [activeImage, setActiveImage] = useState(0);
   const imageContainerRef = useRef<HTMLDivElement>(null);
+  const imagePreviewContainerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const container = imageContainerRef.current;
-    const activeElement = container?.children[activeImage] as HTMLElement;
+    const previewContainer = imagePreviewContainerRef.current;
 
+    const activeElement = container?.children[activeImage] as HTMLElement;
     if (activeElement) {
       activeElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+        inline: 'center',
+      });
+    }
+
+    const previewActiveElement = previewContainer?.children[
+      activeImage
+    ] as HTMLElement;
+    if (previewActiveElement) {
+      previewActiveElement.scrollIntoView({
         behavior: 'smooth',
         block: 'nearest',
         inline: 'center',
@@ -32,20 +44,27 @@ export default function ImagesPreview({ images, title }: Props) {
 
   return (
     <div className='select-none'>
-      <ImagePreviewModal src={images[activeImage]}>
-        <Image
-          as={NextImage}
-          fill
-          src={images[activeImage]}
-          alt={`${title} - ${activeImage + 1}`}
-          sizes='(max-width: 640px) 90vw, (max-width: 1024px) 45vw, 35vw'
-          className='object-cover object-top'
-          classNames={{
-            wrapper: 'w-full aspect-video outline outline-teal-600 !max-w-none',
-          }}
-          isBlurred
-        />
-      </ImagePreviewModal>
+      <div
+        className='w-full aspect-video outline outline-teal-600 rounded-md inline-block whitespace-nowrap transition-all scroll-smooth overflow-hidden'
+        ref={imagePreviewContainerRef}
+      >
+        {images.map((image, index) => (
+          <div key={index} className='inline-block w-full h-full aspect-video'>
+            <ImagePreviewModal src={image}>
+              <Image
+                src={image}
+                alt={`${title} - ${activeImage + 1}`}
+                className='object-cover object-top w-full h-full'
+                classNames={{
+                  wrapper: 'w-full aspect-video !max-w-none',
+                }}
+                isBlurred
+                radius='none'
+              />
+            </ImagePreviewModal>
+          </div>
+        ))}
+      </div>
       <div className='mt-6 overflow-x-auto overflow-y-hidden hide-scrollbar'>
         <div
           className='inline-block whitespace-nowrap transition-all scroll-smooth'
@@ -53,12 +72,9 @@ export default function ImagesPreview({ images, title }: Props) {
         >
           {images.map((image, index) => (
             <Image
-              as={NextImage}
-              fill
               key={index}
               src={image}
               alt={`${title} - ${index + 1}`}
-              sizes='(max-width: 640px) 40vw, (max-width: 1024px) 25vw, 20vw'
               className='w-full h-full object-cover object-top inline-block rounded-none'
               classNames={{
                 wrapper: `h-24 aspect-video inline-block border-2 ml-2 first:ml-0 transition-all overflow-hidden cursor-pointer rounded-lg !max-w-none ${
