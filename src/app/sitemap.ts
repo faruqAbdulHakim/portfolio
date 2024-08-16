7ebd476a0481a117aaf1ca617214dfcfb26aaca1
faruqAbdulHakim <faruqabdulhakim2001@gmail.com>
2024-08-17 05:23:11 +0700

    feat: add experiences detail page into sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -18,6 +18,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 1,
     },
     {
+      url: `${CONFIG.LIVE_SITE_URL}/experiences`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
       url: `${CONFIG.LIVE_SITE_URL}/portfolios`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
